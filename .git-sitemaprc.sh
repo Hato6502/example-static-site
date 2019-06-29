7a0e34f5a41228ae7dc58c48d7d7f6952205bb53
hato6502 <erisuke12345@yahoo.co.jp>
2019-06-29 21:57:22 +0900

    git-sitemap 設定。
    git-sitemap 設定。

--- .git-sitemaprc.sh
+++ .git-sitemaprc.sh
@@ -1,9 +1,10 @@
 PREFIX="https://b-hood.site"
-SINCE="1 month ago"
+SINCE="2019-06-01"
+CURRENT_TIME="2019-06-25"
 
 function loc() {
   # index.html は除去します。
-  cat - | sed "s/index\.html$//g"
+  cat - | grep -E "\.html$" | sed "s/index\.html$//g"
 }
 
 function difftest () {
@@ -14,4 +15,31 @@ function difftest () {
   # css のハッシュ変化は無視します。
   diff=`echo "${diff}" | grep -v "\/css\/app\.css"`
   return `[ -n "${diff}" ]`
+}
+
+function priority() {
+  # $1 file path
+  if [ $1 == `git rev-parse --show-toplevel`/index.html ]; then
+    echo "1.0"
+  else
+    echo "0.5"
+  fi
+}
+
+function lastmod() {
+  # $1 file path
+  if [ $1 == `git rev-parse --show-toplevel`/articles/index.html ]; then
+    date --iso-8601 | w3cdatetime
+  else
+    cat -
+  fi
+}
+
+function changefreq() {
+  # $1 file path
+  if [ $1 == `git rev-parse --show-toplevel`/articles/index.html ]; then
+    echo "always"
+  else
+    cat -
+  fi
 }
