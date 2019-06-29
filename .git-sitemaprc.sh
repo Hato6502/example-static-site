4f310c90cc4fd547f74e0314b68100d323dfd7f1
hato6502 <erisuke12345@yahoo.co.jp>
2019-06-29 21:20:51 +0900

    git 公開用の設定をしました。

--- .git-sitemaprc.sh
+++ .git-sitemaprc.sh
@@ -1,11 +1,17 @@
-prefix="https://b-hood.site"
+PREFIX="https://b-hood.site"
+SINCE="1 month ago"
+
+function loc() {
+  # index.html は除去します。
+  cat - | sed "s/index\.html$//g"
+}
 
 function difftest () {
   # $1 コミットのハッシュ
   # $2 ファイルパス
 
-  local diff=`git diff -U0 $1^..$1 $2 | tail -n +5 | grep -E "^(\+|-)"`
+  local diff=`git diff -U0 $1^..$1 -- $2 | tail -n +5 | grep -E "^(\+|-)"`
   # css のハッシュ変化は無視します。
-  diff=`echo "${diff}" | grep -v "\/css\/app.css"`
+  diff=`echo "${diff}" | grep -v "\/css\/app\.css"`
   return `[ -n "${diff}" ]`
 }
